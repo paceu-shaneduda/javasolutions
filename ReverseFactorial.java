2923084a357b8058a63e49d606ea92cb415c7805
paceu-shaneduda <sd26164n@pace.edu>
2017-02-06 11:28:50 -0500

    Update ReverseFactorial.java

--- ReverseFactorial.java
+++ ReverseFactorial.java
@@ -2,42 +2,29 @@ import java.util.*;
 public class ReverseFactorial {
 
 	public static void main(String[] args) {
+				
+		Scanner scanner = new Scanner(System. in);
+		System.out.println("Enter a number to check its factorial");
 		
-    //Generates random numbers for to test
-		Random rand = new Random();
-		int  testNum1 = rand.nextInt(9999) + 1;
-		int  testNum2 = rand.nextInt(9999) + 1;
-		int  testNum3 = rand.nextInt(9999) + 1;
+		int testNum = scanner.nextInt();
+		System.out.println(testNum + " = " + reverseFact(testNum)+ "!" );
 		
-		/*System.out.println("Test number is "+ testNum1);
-		System.out.println(testNum1 + " = " + reverseFact(testNum1)+ "!" );
-		
-		System.out.println("Test number is "+ testNum2);
-		System.out.println(testNum2 + " = " + reverseFact(testNum2)+ "!" );
-		
-		System.out.println("Test number is "+ testNum3);
-		System.out.println(testNum3 + " = " + reverseFact(testNum3)+ "!" );*/
-		
-    //Set test case
-		System.out.println("Test number is 720");
-		System.out.println("720 equals " + reverseFact(720)+"!");
 		
 		
 	}
 	
+	/*need to figure out 'edge' cases where the testNum does'nt equal a clean factorial (5040, 720, etc)
+	Any quick way to quickly check to see if the inputted number is good?*/
 	public static int reverseFact(int x){
 		
 		int i=1;
-				
+		
 		while(x > 1){
 			
 			i++;
-			x=x/i;
+			x=x/i;	
 			
 		}
-		
-		return i;
-		
+		return i;	
 	}
-
 }
